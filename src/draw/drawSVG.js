9d56b3051e8ea09cb908501a8dac58b8656798f1
Sandeep Roy <31067688+sandeep-roy@users.noreply.github.com>
2026-03-20 08:35:42 +0100

    Create drawSVG.js
    Signed-off-by: Sandeep Roy <31067688+sandeep-roy@users.noreply.github.com>

---
+++ src/draw/drawSVG.js
@@ -0,0 +1,19 @@
+import { state } from "../state.js";
+
+export function drawSVGBase() {
+  const svg = state.svg;
+  svg.innerHTML = `
+    <defs>
+      <clipPath id="plotClip">
+        <rect id="clipRect"></rect>
+      </clipPath>
+    </defs>
+
+    <g id="gridLayer"></g>
+    <g id="yAxisLayer"></g>
+    <g id="axisLayer"></g>
+    <g id="barLayer" clip-path="url(#plotClip)"></g>
+    <g id="xTickTextLayer"></g>
+    <g id="xAxisLabelLayer"></g>
+  `;
+}
