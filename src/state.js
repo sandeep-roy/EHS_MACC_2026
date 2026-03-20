d0e014c77ba7d36da3d5c8b02e85917d69018cb4
Sandeep Roy <31067688+sandeep-roy@users.noreply.github.com>
2026-03-20 08:32:25 +0100

    Create state.js
    Signed-off-by: Sandeep Roy <31067688+sandeep-roy@users.noreply.github.com>

---
+++ src/state.js
@@ -0,0 +1,27 @@
+export const state = {
+  raw: null,
+  rows: [],
+
+  svg: null,
+  tooltip: null,
+
+  scale: 1,
+  translateX: 0,
+
+  layout: {
+    margin: { top: 60, right: 80, bottom: 150, left: 200 },
+    W: 0,
+    H: 0,
+    innerW: 0,
+    innerH: 0
+  },
+
+  scales: {
+    x: null,
+    y: null,
+    y0: 0,
+    totalAbate: 0,
+    minMAC: 0,
+    maxMAC: 0
+  }
+};
