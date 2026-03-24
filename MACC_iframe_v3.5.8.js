57f8297b3214ab320d3155700a3318e547ef69df
Sandeep Roy <31067688+sandeep-roy@users.noreply.github.com>
2026-03-24 15:02:14 +0100

    Update MACC_iframe_v3.5.8.js
    Signed-off-by: Sandeep Roy <31067688+sandeep-roy@users.noreply.github.com>

--- MACC_iframe_v3.5.8.js
+++ MACC_iframe_v3.5.8.js
@@ -93,7 +93,7 @@
     }
 
     _render(){
-      this._frame.src="https://sandeep-roy.github.io/EHS_MACC_2026/iframe.html?v=1.0";
+      this._frame.src="https://sandeep-roy.github.io/EHS_MACC_2026/iframe.html?v=2";
 
       let attempts=0;
       const trySend=()=>{
